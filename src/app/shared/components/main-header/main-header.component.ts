fe7cd3e0668cdc9ff5dad32efd1319653cd7c94e
yahiabakry10 <yehiabakry10@gmail.com>
2026-04-18 22:48:56 +0200

    adding animation directive

--- src/app/shared/components/main-header/main-header.component.ts
+++ src/app/shared/components/main-header/main-header.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-main-header',
-  imports: [],
+  imports: [ScrollRevealDirective],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.css',
 })
-export class MainHeaderComponent {
-
-}
+export class MainHeaderComponent {}
